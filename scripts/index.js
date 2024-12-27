7efe6e16df2e24fee63a95fca2659b90fc175625
AlbertoGarcia98 <albertogh.16@gmail.com>
2024-12-27 12:52:00 +0100

    añadi pais

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,21 @@
  // Lee el archivo data.json y lo muestra en la página
- fetch('data.json')
- .then(response => {
-     if (!response.ok) {
-         throw new Error('Error al leer el archivo JSON');
-     }
-     return response.json();
- })
- .then(data => {
+fetch('data.json')
+.then(response => {
+    if (!response.ok) {
+        throw new Error('Error al leer el archivo JSON');
+    }
+    return response.json();
+})
+.then(data => {
      // Manipula los datos obtenidos
-     const dataDiv = document.getElementById('data');
-     dataDiv.innerHTML = `
-         <p>Nombre: ${data.name}</p>
-         <p>Edad: ${data.age}</p>
-         <p>Ciudad: ${data.city}</p>
-     `;
- })
- .catch(error => {
-     console.error('Error:', error);
- });
+    const dataDiv = document.getElementById('data');
+    dataDiv.innerHTML = `
+        <p>Nombre: ${data.name}</p>
+        <p>Edad: ${data.age}</p>
+        <p>Ciudad: ${data.city}</p>
+        <p>Pais: ${data.pais}</p>
+    `;
+})
+.catch(error => {
+    console.error('Error:', error);
+});
